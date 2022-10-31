c85044062ff09038d000986fcb04d57f5b180eae
Graycot <ggraycot@gmail.com>
2022-10-31 19:44:35 -0400

    Init old-code loop ring

---
+++ sites.js
@@ -0,0 +1,17 @@
+export const sites = [
+    'https://graycot.com/webring/',
+    'https://graystea.neocities.org/',
+    'https://windigo.neocities.org/',
+    'https://vaea-v.neocities.org',
+    'https://www.idi.moe/',
+    'https://seirdy.one/',
+    'https://aroace.space/',
+    'https://bulltown.2022.jenett.org/',
+    'https://rarimena.neocities.org/',
+    'https://tsk.bearblog.dev/',
+    'https://fanlistings.nickifaulk.com/webrings/',
+    'https://johnlins.com/'
+  ];
+
+
+
