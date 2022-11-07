e7eb2db9f76072ba301e61f7d04d19ed2a591b6c
Graycot <ggraycot@gmail.com>
2022-11-07 01:19:04 -0500

    Update to Oring v5.0

--- webring.js
+++ webring.js
@@ -1,4 +1,4 @@
-/* Oring v4.0. Copyleft 🄯 ALL WRONGS RESERVED 🄯 Gray (g@graycot.dev) (https://graycot.dev/).
+/* Oring v5.0. Copyleft 🄯 ALL WRONGS RESERVED 🄯 Gray (g@graycot.dev) (https://graycot.dev/).
 
 Oring is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License (GPLv3+) as published by
@@ -11,7 +11,7 @@ fetch("./webring.json")
 })
 .then(data => webring(data));
 
-// get sites list
+// get member sites list
 fetch("./sites.json")
 .then(response => {
    return response.json();
@@ -20,67 +20,68 @@ fetch("./sites.json")
 
 function webring(data) {
   // get webring data webring.json
-  let webringName = data.webringInfo[0].webringName;
-  let webringURL = data.webringInfo[0].webringURL;
-  let webringHome = data.webringInfo[0].webringHome;
-  let webringMemberList = data.webringInfo[0].webringMemberList;
+  var webringName = data.webringInfo[0].webringName;
+  var webringURL = data.webringInfo[0].webringURL;
+  var webringHome = data.webringInfo[0].webringHome;
+  var webringMemberList = data.webringInfo[0].webringMemberList;
 }
 
 function sites(data) {
+  // get sub.domain.TLD of referrer member site.
+  var referrerURL = document.referrer;
+  // Remove http(s) schemes and www. subdomain from URL.
+  var referrerURLReplace = referrerURL.replace(/http(s)?(:)?(\/\/)?|(\/\/)?(www\.)?/g, "");
 
-  // get URL of this member site
-  let thisSiteURL = window.location.href;
-  //init to avoid weird errors
-  let thisIndex;
-  let i;
-  let thisSiteName
-  //find this site in member list
+  // find referrer member site in member list.
   for (i = 0; i < data.webringSites.length; i++) {
-    if (thisSiteURL.startsWith(data.webringSites[i].siteURL)) {
-      thisIndex = i;
-      thisSiteName = data.webringSites[thisIndex].siteName;
+    siteURLRaw = data.webringSites[i].siteURL;
+    // Chop off everything past the TLD.
+    siteURLMatch = siteURLRaw.match(/^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)/igm, "");
+    // Remove http(s) schemes and www. subdomain from URL.
+    siteURLReplace = siteURLMatch[0].replace(/http(s)?(:)?(\/\/)?|(\/\/)?(www\.)?/g, "");
+
+    if (referrerURLReplace.startsWith(siteURLReplace)) {
+      var referrerIndex = i;
+      var referrerSiteURL = data.webringSites[referrerIndex].siteURL;
+      var referrerSiteName = data.webringSites[referrerIndex].siteName;
       break;
     }
   }
-  //if this site is not in member list, set index to last member of list
-  if (thisIndex == undefined) {
-  thisIndex = data.webringSites.length-1;
-  thisSiteName = 'unknown'
-  }
-  // find index of site before and after this site. Also compute a random index.
-  let previousIndex = (thisIndex-1 < 0) ? data.webringSites.length-1 : thisIndex-1;
-  let randomIndex = Math.floor(Math.random() * (data.webringSites.length));
-  let nextIndex = (thisIndex+1 >= data.webringSites.length) ? 0 : thisIndex+1;
-  // use the indices calculated above to find the corresponding site URL in the member list
-  let previousSiteURL = data.webringSites[previousIndex].siteURL;
-  let randomSiteURL = data.webringSites[randomIndex].siteURL;
-  let nextSiteURL = data.webringSites[nextIndex].siteURL;
-
-  // Detects whether user clicked the Previous, List, Home, Next, Random, or other link:
+  // Detect whether visitor clicked the Previous, List, Home, Next, Random, or other link:
   const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
   });
   let value = params.action;
 
-  // If the site that the user just came from is not part of the webring, this sets the Previous and Next button to Random.
-  if (thisIndex == null) {
-    previousIndex = randomIndex;
-    nextIndex = randomIndex;
-  }
+  // If the referrer site is not part of the webring, set the previous and next values to random.
 
-  // Previous, List, Home, Next, Random, or other actions
-  if (value == 'prev') {
+  // Execute redirect upon Previous, List, Home, Next, Random, or other actions
+  if (value == 'prev' && referrerIndex !== undefined) {
+      //find previous site in member list
+      let previousIndex = (referrerIndex-1 < 0) ? data.webringSites.length-1 : referrerIndex-1;
+      let previousSiteURL = data.webringSites[previousIndex].siteURL;
       window.location.href = previousSiteURL;
-  } else if (value == 'next') {
+
+  } else if (value == 'next' && referrerIndex !== undefined) {
+      //find next site in member list
+      let nextIndex = (referrerIndex+1 >= data.webringSites.length) ? 0 : referrerIndex+1;
+      let nextSiteURL = data.webringSites[nextIndex].siteURL;
       window.location.href = nextSiteURL;
+
   } else if (value == 'list') {
       window.location.href = webringMemberList;
+
   } else if (value == 'home') {
       window.location.href = webringHome;
-  } else if (value == 'rand') {
-      window.location.href = randomSiteURL;
-  } else {
-      window.location.href = randomSiteURL; //In-case of value == null
-  }
 
+  } else if (value == 'test') {
+      console.log('test');
+
+  } else {
+      //In-case of value == undefined or referrerIndex is undefined, find random site in member list
+      let randomIndex = Math.floor(Math.random() * (data.webringSites.length));
+      let randomSiteURL = data.webringSites[randomIndex].siteURL;
+      let randomSiteName = data.webringSites[randomIndex].siteName;
+      window.location.href = randomSiteURL;
+  }
 };
